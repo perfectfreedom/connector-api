296dde00ff28ac47f1478dee8bd4e65ab5baad1a
perfectfreedom <russbartlett623@gmail.com>
2022-09-12 22:09:44 -0500

    add new data

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 8000
 app.use(cors())
 
 const setlists = {
-        "setlist" : [ {
+        "firstuser" : [ {
+          "setlist" : [ {
           "artist" : {
             "mbid" : "b10bbbfc-cf9e-42e0-be17-e2c3e1d2600d",
             "tmid" : 735610,
@@ -76,6 +77,82 @@ const setlists = {
         "total" : 42,
         "page" : 1,
         "itemsPerPage" : 20
+      } ],
+
+      "seconduser" : [ {
+        "setlist" : [ {
+        "artist" : {
+          "mbid" : "b10bbbfc-cf9e-42e0-be17-e2c3e1d2600d",
+          "tmid" : 735610,
+          "name" : "The Beatles",
+          "sortName" : "Beatles, The",
+          "disambiguation" : "John, Paul, George and Ringo",
+          "url" : "https://www.setlist.fm/setlists/the-beatles-23d6a88b.html"
+        },
+        "venue" : {
+          "city" : { },
+          "url" : "https://www.setlist.fm/venue/compaq-center-san-jose-ca-usa-6bd6ca6e.html",
+          "id" : "6bd6ca6e",
+          "name" : "Compaq Center"
+        },
+        "tour" : {
+          "name" : "North American Tour 1964"
+        },
+        "set" : [ {
+          "name" : "...",
+          "encore" : 12345,
+          "song" : [ { }, { } ]
+        }, {
+          "name" : "...",
+          "encore" : 12345,
+          "song" : [ { }, { } ]
+        } ],
+        "info" : "Recorded and published as 'The Beatles at the Hollywood Bowl'",
+        "url" : "https://www.setlist.fm/setlist/the-beatles/1964/hollywood-bowl-hollywood-ca-63de4613.html",
+        "id" : "63de4613",
+        "versionId" : "7be1aaa0",
+        "eventDate" : "23-06-1964",
+        "lastUpdated" : "2013-10-20T05:18:08.000+0000"
+      }, {
+        "artist" : {
+          "mbid" : "...",
+          "tmid" : 12345,
+          "name" : "...",
+          "sortName" : "...",
+          "disambiguation" : "...",
+          "url" : "..."
+        },
+        "venue" : {
+          "city" : { },
+          "url" : "...",
+          "id" : "...",
+          "name" : "..."
+        },
+        "tour" : {
+          "name" : "..."
+        },
+        "set" : [ {
+          "name" : "...",
+          "encore" : 12345,
+          "song" : [ { }, { } ]
+        }, {
+          "name" : "...",
+          "encore" : 12345,
+          "song" : [ { }, { } ]
+        } ],
+        "info" : "...",
+        "url" : "...",
+        "id" : "...",
+        "versionId" : "...",
+        "eventDate" : "...",
+        "lastUpdated" : "..."
+      } ],
+      "total" : 42,
+      "page" : 1,
+      "itemsPerPage" : 20
+    } ]
+
+      
 }
 
 app.get('/', (request, response)=>{
